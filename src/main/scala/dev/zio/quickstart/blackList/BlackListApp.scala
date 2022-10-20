0df2649a0690ae8e2b1fa3d39c29910789cc8700
Николай Зотин <15nk33l49@gmail.com>
2022-10-20 14:07:19 +0300

    add code BlackListApp

--- src/main/scala/dev/zio/quickstart/blackList/BlackListApp.scala
+++ src/main/scala/dev/zio/quickstart/blackList/BlackListApp.scala
@@ -4,8 +4,25 @@ import zhttp.http.*
 import zio.*
 import zio.json.*
 
-object BlackListApp {
-  
-  
+object BlackListApp :
 
-}
+  def apply(): Http[Any, Throwable, Request, Response] =
+    Http.collectZIO[Request] {
+      // POST /users -d '{"name": "John", "age": 35}'
+      case req@(Method.POST -> !! / "transaction-check") =>
+        for
+          u <- req.bodyAsString.map(_.fromJson[InputPorts])
+          r <- ZIO.fromEither(u)
+            case Left(e) =>
+              ZIO.debug(s"Failed to parse the input: $e").as(
+                Response.text(e).setStatus(Status.BadRequest)
+              )
+            case Right(u) =>
+              Response.text("Good request!")
+        yield r
+    }
+
+
+
+
+
